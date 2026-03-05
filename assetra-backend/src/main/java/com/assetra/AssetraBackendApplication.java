c8be2692d23f52e7e729b1513a4a8ca8c5d799ff
Shazmina <shxminaoud@gmail.com>
2026-03-05 12:17:40 +0530

    chore: initial backend setup, folder structure and dependencies

---
+++ assetra-backend/src/main/java/com/assetra/AssetraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assetra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetraBackendApplication.class, args);
+	}
+
+}
---
+++ assetra-backend/src/test/java/com/assetra/AssetraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assetra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
