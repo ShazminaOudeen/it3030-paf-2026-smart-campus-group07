de5ed67f36829d4cdd8e33a7b1efc6d64135051c
Aathika <it23546202@my.sliit.lk>
2026-04-24 16:53:47 +0530

    Add AuthResponse DTO - Member 4

---
+++ assetra-backend/src/main/java/com/assetra/notification/dto/AuthResponse.java
@@ -0,0 +1,15 @@
+package com.assetra.notification.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String email;
+    private String name;
+    private String role;
+    private String picture;
+    private String id;
+}
