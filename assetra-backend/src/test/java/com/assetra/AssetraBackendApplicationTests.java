eef5b55cd9fd3c5a0a63d3faee13c9bcd2ac0d0f
Aathika <it23546202@my.sliit.lk>
2026-04-25 18:58:23 +0530

    fix: add test profile to fix CI build

--- assetra-backend/src/test/java/com/assetra/AssetraBackendApplicationTests.java
+++ assetra-backend/src/test/java/com/assetra/AssetraBackendApplicationTests.java
@@ -2,12 +2,13 @@ package com.assetra;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AssetraBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
