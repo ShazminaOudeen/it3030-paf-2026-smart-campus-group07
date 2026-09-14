d93498efe7eaf02d8fbcb6cb99b7df4bbea28077
Shazmina <it23533714@my.sliit.lk>
2026-09-14 20:52:42 +0530

    fixing cd/ci breaking issue

--- assetra-backend/src/main/java/com/assetra/notification/repository/NotificationRepository.java
+++ assetra-backend/src/main/java/com/assetra/notification/repository/NotificationRepository.java
@@ -4,12 +4,12 @@ import com.assetra.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 import java.util.UUID;
 
-@Repository
+
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
 
     // Get all notifications for a user, newest first
