1bf903c4de9b8c20167f989a35f7c09c1495d8cc
Shazmina <it23533714@my.sliit.lk>
2026-09-14 21:06:22 +0530

    fixing playwright issue thats breaking the CI/CD

--- playwright.config.js
+++ playwright.config.js
@@ -14,6 +14,7 @@ export default defineConfig({
   },
 
      webServer: {
+      cwd: './frontend',
     command: 'npm run dev',
     url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
